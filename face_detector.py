5e936f7f5e4a79539f5d64f02ae2b4f1293ba406
Jake Mooney <jrmooney@gmail.com>
2020-02-25 17:32:01 +0200

    updates to chrome extension allowing user to turn it off and on as well as making it run on a page refresh

--- face_detector.py
+++ face_detector.py
@@ -1,14 +1,15 @@
 from keras.models import load_model
 import cv2
 import numpy as np
+import time
 
 class FaceDetector:
 	def __init__(self):
 		pass
 
 	def get_face_and_predict(self):
+
 		face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-		model = load_model('model_faces.h5')
 
 		cap = cv2.VideoCapture(0)
 		for i in range(30):
@@ -16,7 +17,7 @@ class FaceDetector:
 		gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		# plt.imshow(gray_image)
 		faces_rects = face_cascade.detectMultiScale(gray_image, 1.1, 4)
-		cv2.waitKey(150)
+		# cv2.waitKey(150)
 		for (x, y, w, h) in faces_rects:
 		    cv2.rectangle(gray_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 		    img = gray_image[y:y + h, x:x + w]
@@ -24,7 +25,9 @@ class FaceDetector:
 		img = cv2.resize(src=img, dsize=(48,48))
 		img = np.expand_dims(np.expand_dims(img, 0), 3)
 		cap.release()
-
+		model = load_model('model_faces.h5')
+		start = time.time()
 		prediction = model.predict(img)
-
+		end = time.time()
+		print(f"PREDICTIONTIME: {end-start}")
 		return prediction[0][0]
--- flask_app.py
+++ flask_app.py
@@ -5,9 +5,12 @@ from flask_cors import CORS
 from text_sentiment import *
 from face_detector import FaceDetector
 from keras.models import load_model
+import time
 
 app = Flask(__name__)
 CORS(app)
+ts = TextSentiment()
+face_detector = FaceDetector()
 
 
 @app.route('/')
@@ -20,12 +23,11 @@ def hello():
 def get_sentiments():
 	if request.method == 'POST':
 		#get page sentiment
-		ts = TextSentiment()
-		sentiment = ts.get_sentiment(request.data)
-		#get the face emotion score
-		face_detector = FaceDetector()
-		face_score = face_detector.get_face_and_predict()
+		# sentiment = ts.get_sentiment(request.data)
+		sentiment = -1
 
+		#get the face emotion score
+		face_score = face_detector.get_face_and_predict()
 		if face_score > 0.3:
 			print(f"FACE SCORE IS SAD. SCORE: {face_score}")
 			if sentiment < 0:
@@ -35,7 +37,7 @@ def get_sentiments():
 				print(f"PAGE SCORE IS NOT SAD. SCORE: {sentiment}")
 				return json.dumps({'sentiment_score': 1})
 		else:
-			print(f"FACE SCORE WAS NOT SAD. SCORE: {sentiment}")
+			print(f"FACE SCORE WAS NOT SAD. SCORE: {face_score}")
 			return json.dumps({'sentiment_score': 1})
 
 
